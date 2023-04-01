889a24ae63ee9a24ee6b6dd3b077f2fd90ba5e0b
LilacVenus <66577598+lilacvenus@users.noreply.github.com>
2023-04-01 18:13:57 -0300

    Added Map Names + Writing Messages

--- Parser.js
+++ Parser.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 
 const folderPath = './MapFiles';
 const languages = ["Français"];
+let untranslatedMsgs = ["\\m[Rim]There's nothing to smell here.", "\\m[Rim]I probably shouldn't do that."]; // temporary hard coded strings
+let mapNames = [];
 
 fs.readdir(folderPath, (err, files) => {
     if (err) {
@@ -9,26 +11,51 @@ fs.readdir(folderPath, (err, files) => {
         return;
     }
 
-    let translations = { msg: {}, cmd: {}, terms: {}, custom: {} };
+    let translations = { msg: {}, cmd: {}, terms: {}, custom: {} }; // Empty translations template
 
-    const newTranslation = "\\m[Rim]There's nothing to smell here."; // Temporary hard coded string
+    // Read all parameters here
 
-    languages.forEach(language => {
-        translations.msg[newTranslation] = translations.msg[newTranslation] || {}; // Make key
-        translations.msg[newTranslation][language] = ""; // Write empty translation strings
-      });
+    // End of reading parameters
 
-    fs.writeFile('Translations.json', JSON.stringify(translations, null, 2), (err) => {
-        if (err) throw err;
-        console.log('The file has been saved!');
-      });
+    // Read and add maps names to custom translations
 
-    //   // filter files that match the format "Map000.json"
-    //   const regex = /^Map\d{3}\.json$/;
-    //   const matchingFiles = files.filter(file => regex.test(file));
+    fs.readFile('MapFiles/MapInfos.json', (err, data) => {
+        if (err) {
+            console.log("Could not read the MapInfos.json file");
+            return;
+        }
+        const mapData = JSON.parse(data);
 
-    //   // output the file names of the matching files
-    //   matchingFiles.forEach(file => {
-    //     console.log(file);
-    //   });
+        mapData.forEach(item => {
+            if (item && "name" in item) {
+                mapNames.push(item.name);
+            }
+        });
+
+        // Write Data to translations variable
+        mapNames.forEach(mapName => { // All the map names from MapInfos.json
+            languages.forEach(language => {
+                translations.custom[mapName] = translations.custom[mapName] || {}; 
+                translations.custom[mapName][language] = "";
+            });
+        });
+
+        untranslatedMsgs.forEach(newTranslation => { // All the dialogue from Map000.json files
+            languages.forEach(language => {
+                translations.msg[newTranslation] = translations.msg[newTranslation] || {};
+                translations.msg[newTranslation][language] = "";
+            });
+        });
+
+
+        // Write translations to file
+        fs.writeFile('Translations.json', JSON.stringify(translations, null, 2), (err) => {
+            if (err) throw err;
+            console.log('The file has been saved!');
+        });
+    });
+
+    // filter files that match the format "Map000.json"
+    // const regex = /^Map\d{3}\.json$/;
+    // const matchingFiles = files.filter(file => regex.test(file));
 });
