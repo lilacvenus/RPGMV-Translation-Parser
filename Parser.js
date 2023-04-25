26d929ab8d66a7549a4003a5cb1f73dc11b20acc
LilacVenus <66577598+lilacvenus@users.noreply.github.com>
2023-04-25 14:49:21 -0300

    Menu commands added

--- Parser.js
+++ Parser.js
@@ -1,10 +1,11 @@
 const fs = require('fs');
 
 const folderPath = './MapFiles';
-const languages = ["Français", "Spanish"];
-//let untranslatedMsgs = ["\\m[Rim]There's nothing to smell here.", "\\m[Rim]I probably shouldn't do that."]; // temporary hard coded strings
+const languages = ["Français"]; // Adding more language auto-generates the fields.
 let untranslatedMsgs = [];
+let untranslatedCmds = [];
 let mapNames = [];
+
 
 fs.readdir(folderPath, (err, files) => {
     if (err) {
@@ -14,26 +15,11 @@ fs.readdir(folderPath, (err, files) => {
 
     let translations = { msg: {}, cmd: {}, terms: {}, custom: {} }; // Empty translations template
 
-    // Read all parameters here
     const regex = /^Map\d{3}\.json$/; //filter files that match the format "Map000.json"
     const matchingFiles = files.filter(file => regex.test(file)); // array of files that match the format "Map000.json"
 
-    console.log("Found " + matchingFiles.length + " files");
-
-    const totalFiles = matchingFiles.length;
-    let processedFiles = 0;
-
-    function progressBar(current, total) {
-        const percent = (current / total) * 100;
-        const filled = Math.floor(percent / 5);
-        const empty = 20 - filled;
-        const progressBar = "[" + "#".repeat(filled) + " ".repeat(empty) + "]";
-        console.clear();
-        console.log("Processing Dialogue: " + progressBar + " " + current + "/" + total + " - " + percent.toFixed(2) + "%");
-    }
-
     matchingFiles.forEach(file => {
-        const json = require(`./MapFiles/${file}`); // Load the JSON files
+        const json = require(`./MapFiles/${file}`);
 
         // Read and add dialogue to custom translations that has a 401 code
         json.events.forEach(event => {
@@ -41,10 +27,19 @@ fs.readdir(folderPath, (err, files) => {
                 event.pages.forEach(page => {
                     if (page && page.list) {
                         page.list.forEach(item => {
+                            // Dialogue
                             if (item.code === 401 && Array.isArray(item.parameters)) {
                                 item.parameters.forEach(param => {
                                     if (typeof param === 'string') {
                                         untranslatedMsgs.push(param);
+                                    }
+                                });
+                            }
+                            // Menu options
+                            else if (item.code === 102 && Array.isArray(item.parameters) && Array.isArray(item.parameters[0])) { // Updated condition
+                                item.parameters[0].forEach(param => { // Iterate through nested array
+                                    if (typeof param === 'string') {
+                                        untranslatedCmds.push(param);
                                     }
                                 });
                             }
@@ -53,12 +48,7 @@ fs.readdir(folderPath, (err, files) => {
                 });
             }
         });
-        
-        progressBar(++processedFiles, totalFiles);
     });
-
-
-    // End of reading parameters
 
     // Read and add maps names to custom translations
 
@@ -76,19 +66,9 @@ fs.readdir(folderPath, (err, files) => {
         });
 
         // Write Data to translations variable
-        mapNames.forEach(mapName => { // All the map names from MapInfos.json
-            languages.forEach(language => {
-                translations.custom[mapName] = translations.custom[mapName] || {};
-                translations.custom[mapName][language] = "UNTRANSLATED-MAP-NAME";
-            });
-        });
-
-        untranslatedMsgs.forEach(newTranslation => { // All the dialogue from Map000.json files
-            languages.forEach(language => {
-                translations.msg[newTranslation] = translations.msg[newTranslation] || {};
-                translations.msg[newTranslation][language] = "UNTRANSLATED-DIALOGUE";
-            });
-        });
+        translate(translations, mapNames, 'custom', 'UNTRANSLATED-MAP-NAME');
+        translate(translations, untranslatedMsgs, 'msg', 'UNTRANSLATED-DIALOGUE');
+        translate(translations, untranslatedCmds, 'cmd', 'UNTRANSLATED-COMMAND');
 
 
         // Write translations to file
@@ -97,6 +77,13 @@ fs.readdir(folderPath, (err, files) => {
             console.log('Successfully wrote to Translations.json');
         });
     });
-
-
 });
+
+const translate = (translations, array, type, defaultValue) => {
+    array.forEach(newTranslation => {
+        languages.forEach(language => {
+            translations[type][newTranslation] = translations[type][newTranslation] || {};
+            translations[type][newTranslation][language] = defaultValue;
+        });
+    });
+};
