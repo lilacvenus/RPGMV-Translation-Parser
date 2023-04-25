dbd5b9d6cf09184d9878e5f6bfa89c93365fadb3
LilacVenus <66577598+lilacvenus@users.noreply.github.com>
2023-04-25 16:08:14 -0300

    Fixed scraping + changed default output
    Parser now correctly handles multi-line messages, and outputs the english text to ensure the formatting works as expected.

--- Parser.js
+++ Parser.js
@@ -21,23 +21,35 @@ fs.readdir(folderPath, (err, files) => {
     matchingFiles.forEach(file => {
         const json = require(`./MapFiles/${file}`);
 
-        // Read and add dialogue to custom translations that has a 401 code
+        let concatenatedString = "";
+
         json.events.forEach(event => {
             if (event && event.pages) {
                 event.pages.forEach(page => {
                     if (page && page.list) {
-                        page.list.forEach(item => {
-                            // Dialogue
-                            if (item.code === 401 && Array.isArray(item.parameters)) {
-                                item.parameters.forEach(param => {
-                                    if (typeof param === 'string') {
-                                        untranslatedMsgs.push(param);
+                        page.list.forEach((item, index) => {
+                            if (item.code === 401) {
+                                if (item.parameters && item.parameters.length > 0) {
+                                    let parameter = item.parameters[0].trim();
+                                    if (concatenatedString !== "") {
+                                        concatenatedString += "\n" + parameter;
+                                    } else {
+                                        concatenatedString = parameter;
                                     }
-                                });
+                                }
+                            } else {
+                                if (concatenatedString !== "") {
+                                    untranslatedMsgs.push(concatenatedString);
+                                    concatenatedString = "";
+                                }
                             }
-                            // Menu options
-                            else if (item.code === 102 && Array.isArray(item.parameters) && Array.isArray(item.parameters[0])) { // Updated condition
-                                item.parameters[0].forEach(param => { // Iterate through nested array
+                            if (index === page.list.length - 1 && concatenatedString !== "") {
+                                untranslatedMsgs.push(concatenatedString);
+                                concatenatedString = "";
+                            }
+
+                            if (item.code === 102 && Array.isArray(item.parameters) && Array.isArray(item.parameters[0])) {
+                                item.parameters[0].forEach(param => {
                                     if (typeof param === 'string') {
                                         untranslatedCmds.push(param);
                                     }
@@ -48,11 +60,13 @@ fs.readdir(folderPath, (err, files) => {
                 });
             }
         });
+
+
     });
 
-    // Read and add maps names to custom translations
 
-    fs.readFile('MapFiles/MapInfos.json', (err, data) => {
+
+    fs.readFile('MapFiles/MapInfos.json', (err, data) => { // Read and add maps names to custom translations
         if (err) {
             console.log("Could not read the MapInfos.json file");
             return;
@@ -65,10 +79,10 @@ fs.readdir(folderPath, (err, files) => {
             }
         });
 
-        // Write Data to translations variable
-        translate(translations, mapNames, 'custom', 'UNTRANSLATED-MAP-NAME');
-        translate(translations, untranslatedMsgs, 'msg', 'UNTRANSLATED-DIALOGUE');
-        translate(translations, untranslatedCmds, 'cmd', 'UNTRANSLATED-COMMAND');
+        // Write Data to translations variable before exporting as JSON
+        translate(translations, mapNames, 'custom');
+        translate(translations, untranslatedMsgs, 'msg');
+        translate(translations, untranslatedCmds, 'cmd');
 
 
         // Write translations to file
@@ -79,11 +93,11 @@ fs.readdir(folderPath, (err, files) => {
     });
 });
 
-const translate = (translations, array, type, defaultValue) => {
+const translate = (translations, array, type) => {
     array.forEach(newTranslation => {
         languages.forEach(language => {
             translations[type][newTranslation] = translations[type][newTranslation] || {};
-            translations[type][newTranslation][language] = defaultValue;
+            translations[type][newTranslation][language] = newTranslation;
         });
     });
 };
