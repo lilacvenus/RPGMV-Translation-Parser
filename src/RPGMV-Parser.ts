5d1c34c4b7cbe417dc589b7293327d5af033064e
LilacVenus <66577598+lilacvenus@users.noreply.github.com>
2023-06-01 23:54:03 -0300

    Split everything into an array instead of deleting from existence

--- src/RPGMV-Parser.ts
+++ src/RPGMV-Parser.ts
@@ -7,63 +7,40 @@ const languages: string[] = ["Français"];
 const mapfile_folder: string = "./MapFiles/";
 const matching_files = readdirSync(mapfile_folder).filter(file => file.match(/^Map\d{3}\.json$/)); // Regex to match MAP000.json files
 
-function replacePatterns(input: string): string {
-    // Remove "\\C[21]"
-    const step1 = input.replace(/\\C\[\d+\]/g, '');
+function replacePatterns(input: string): string[] {
+    const step1 = input.split(/(\\C\[\d+\])/);             // Split on "\\C[21]"
+    const step2 = step1.flatMap(item => item.split(/(\\fb)/));                   // Split on "\\fb"
+    const step3 = step2.flatMap(item => item.split(/(\\msgposy\[\d+\])/));       // Split on "\\msgposy[455]"
+    const step4 = step3.flatMap(item => item.split(/(\\msgwidth\[[^\]]+\])/));   // Split on "\\msgwidth[auto]"
+    const step5 = step4.flatMap(item => item.split(/(\\lsoff)/));                // Split on "\\lsoff"
+    const step6 = step5.flatMap(item => item.split(/(\\>)/));                    // Split on "\\>"
+    const step7 = step6.flatMap(item => item.split(/(\\m\[[^\]]+\])/));          // Split on "\\m[Cerio]"
+    const step8 = step7.flatMap(item => item.split(/(\\autoevent\[\d+\])/));     // Split on "\\autoevent[1]"
+    const step9 = step8.flatMap(item => item.split(/(\\ntc<[^>]+>)/));           // Split on "\\ntc<Construction Worker>"
+    const step10 = step9.flatMap(item => item.split(/(\\fn<[^>]+>)/));           // Split on "\\fn<Gabriola>"
+    const step11 = step10.flatMap(item => item.split(/(\\TA\[\d+\])/));          // Split on "\\TA[1]"
+    const step12 = step11.flatMap(item => item.split(/(\\lson)/));               // Split on "\\lson"
+    const step13 = step12.flatMap(item => item.split(/(\\\|)/));                 // Split on "\\|"
+    const step14 = step13.flatMap(item => item.split(/(\\\^)/));                 // Split on "\\^"
+    const step15 = step14.flatMap(item => item.split(/(\\\.\n)/));               // Split on "\\.\n"
 
-    // Remove "\\fb"
-    const step2 = step1.replace(/\\fb/g, '');
-
-    // Remove "\\msgposy[455]"
-    const step3 = step2.replace(/\\msgposy\[\d+\]/g, '');
-
-    // Remove "\\msgwidth[auto]"
-    const step4 = step3.replace(/\\msgwidth\[[^\]]+\]/g, '');
-
-    // Remove "\\lsoff"
-    const step5 = step4.replace(/\\lsoff/g, '');
-
-    // Remove "\\>"
-    const step6 = step5.replace(/\\>/g, '');
-
-    // Remove "\\m[Cerio]"
-    const step7 = step6.replace(/\\m\[[^\]]+\]/g, '');
-
-    // Remove "\\autoevent[1]"
-    const step8 = step7.replace(/\\autoevent\[\d+\]/g, '');
-
-    // Remove "\\ntc<Construction Worker>"
-    const step9 = step8.replace(/\\ntc<[^>]+>/g, '');
-
-    // Remove "\\fn<Gabriola>"
-    const step10 = step9.replace(/\\fn<[^>]+>/g, '');
-
-    // Remove "\\TA[1]"
-    const step11 = step10.replace(/\\TA\[\d+\]/g, '');
-
-    // Remove "\\lson"
-    const step12 = step11.replace(/\\lson/g, '');
-
-    // Remove "\\|"
-    const step13 = step12.replace(/\\\|/g, '');
-
-    // Remove "\\^"
-    const step14 = step13.replace(/\\\^/g, '');
-
-    // Remove "\\."
-    const step15 = step14.replace(/\\\./g, '');
-
-    return step15;
+    return step15.filter(item => item.trim().length > 0);
 }
+
 
 
 
 let scraped_data = ScrapeAll(mapfile_folder, matching_files);
 let output_JSON = TranslateAll(languages, scraped_data);
 
+let condition = 1;
 for (let key in output_JSON.msg) {
+    condition++;
     if (output_JSON.msg.hasOwnProperty(key)) {
         console.log(replacePatterns(key));
+    }
+    if (condition > 10) {
+        break;
     }
 }
 
