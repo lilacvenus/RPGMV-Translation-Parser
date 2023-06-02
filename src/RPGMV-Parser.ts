5a882a7b6e94a357a1a154fc873c580a7661127c
LilacVenus <66577598+lilacvenus@users.noreply.github.com>
2023-06-02 00:08:04 -0300

    Organized Crime

--- src/RPGMV-Parser.ts
+++ src/RPGMV-Parser.ts
@@ -7,23 +7,25 @@ const languages: string[] = ["Français"];
 const mapfile_folder: string = "./MapFiles/";
 const matching_files = readdirSync(mapfile_folder).filter(file => file.match(/^Map\d{3}\.json$/)); // Regex to match MAP000.json files
 
-function replacePatterns(input: string): string[] {
-    const step1 = input.split(/(\\C\[\d+\])/);             // Split on "\\C[21]"
-    const step2 = step1.flatMap(item => item.split(/(\\fb)/));                   // Split on "\\fb"
-    const step3 = step2.flatMap(item => item.split(/(\\msgposy\[\d+\])/));       // Split on "\\msgposy[455]"
-    const step4 = step3.flatMap(item => item.split(/(\\msgwidth\[[^\]]+\])/));   // Split on "\\msgwidth[auto]"
-    const step5 = step4.flatMap(item => item.split(/(\\lsoff)/));                // Split on "\\lsoff"
-    const step6 = step5.flatMap(item => item.split(/(\\>)/));                    // Split on "\\>"
-    const step7 = step6.flatMap(item => item.split(/(\\m\[[^\]]+\])/));          // Split on "\\m[Cerio]"
-    const step8 = step7.flatMap(item => item.split(/(\\autoevent\[\d+\])/));     // Split on "\\autoevent[1]"
+function replacePatterns(step0: string): string[] {
+    const step1 = step0.split(/(\\C\[\d+\])/);                                   // Split on "\\C[21]"
+    const step2 = step1.flatMap(item => item.split(/(\\>)/));                    // Split on "\\>"
+    const step3 = step2.flatMap(item => item.split(/(\\fb)/));                   // Split on "\\fb"
+    const step4 = step3.flatMap(item => item.split(/(\\\|)/));                 // Split on "\\|"
+    const step5 = step4.flatMap(item => item.split(/(\\\^)/));                 // Split on "\\^"
+    const step6 = step5.flatMap(item => item.split(/(\\lsoff)/));                // Split on "\\lsoff"
+    const step7 = step6.flatMap(item => item.split(/(\\\.\n)/));               // Split on "\\.\n"
+    const step8 = step7.flatMap(item => item.split(/(\\lson)/));               // Split on "\\lson"
     const step9 = step8.flatMap(item => item.split(/(\\ntc<[^>]+>)/));           // Split on "\\ntc<Construction Worker>"
     const step10 = step9.flatMap(item => item.split(/(\\fn<[^>]+>)/));           // Split on "\\fn<Gabriola>"
-    const step11 = step10.flatMap(item => item.split(/(\\TA\[\d+\])/));          // Split on "\\TA[1]"
-    const step12 = step11.flatMap(item => item.split(/(\\lson)/));               // Split on "\\lson"
-    const step13 = step12.flatMap(item => item.split(/(\\\|)/));                 // Split on "\\|"
-    const step14 = step13.flatMap(item => item.split(/(\\\^)/));                 // Split on "\\^"
-    const step15 = step14.flatMap(item => item.split(/(\\\.\n)/));               // Split on "\\.\n"
-
+    const step11 = step10.flatMap(item => item.split(/(\\m\[[^\]]+\])/));          // Split on "\\m[Cerio]"
+    const step12 = step11.flatMap(item => item.split(/(\\TA\[\d+\])/));          // Split on "\\TA[1]"
+    const step13 = step12.flatMap(item => item.split(/(\\msgposy\[\d+\])/));       // Split on "\\msgposy[455]"
+    const step14 = step13.flatMap(item => item.split(/(\\autoevent\[\d+\])/));     // Split on "\\autoevent[1]"
+    const step15 = step14.flatMap(item => item.split(/(\\msgwidth\[[^\]]+\])/));   // Split on "\\msgwidth[auto]"
+    
+    
+    
     return step15.filter(item => item.trim().length > 0);
 }
 
