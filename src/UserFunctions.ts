3651fff7ec981ca66c22a3b6567ca947a0a14a21
LilacVenus <66577598+lilacvenus@users.noreply.github.com>
2023-06-03 12:51:54 -0300

    Added documentation

--- src/UserFunctions.ts
+++ src/UserFunctions.ts
@@ -5,6 +5,11 @@ import { TranslateAll } from './TranslateFunctions.js';
 dotnev.config();
 const API_KEY = process.env.API_KEY;
 
+// +=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+//
+//  Splits all escape characters + trailing/leading spaces into their own elements  //
+//  Input: String of RPGMV dialogue                                                 //
+//  Output: Array of ever segment split into their own elements                     //
+// +=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+//
 export function splitDialogue(step0: string): string[] {
     const step1 = step0.split(/(\\C\[\d+\])/);                                      // Split on "\\C[21]"
     const step2 = step1.flatMap(item => item.split(/\\n/));                         // Split on "\n"
@@ -23,7 +28,7 @@ export function splitDialogue(step0: string): string[] {
     const step15 = step14.flatMap(item => item.split(/(\\autoevent\[\d+\])/));      // Split on "\\autoevent[1]"
     const step16 = step15.flatMap(item => item.split(/(\\msgwidth\[[^\]]+\])/));    // Split on "\\msgwidth[auto]"
 
-    const step17 = step16.flatMap(item => {
+    const step17 = step16.flatMap(item => {                                        // Split on leading/trailing spaces
         const trimmedItem = item.trim();
         const leadingSpaces = (item.match(/^\s*/) ?? [""])[0];
         const trailingSpaces = (item.match(/\s*$/) ?? [""])[0];
@@ -45,6 +50,11 @@ export function splitDialogue(step0: string): string[] {
     return step17.filter(item => item.length > 0);
 }
 
+// +=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+//
+//  Uses DeepL API to translate a string                                            //
+//  Input: String to be translated and the target language code                     //
+//  Output: Translated string                                                       //
+// +=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+//
 export async function translateAPI(text: string, target_lang: string): Promise<string> {
     const encoded_text = `text=${encodeURIComponent(text)}&target_lang=${encodeURIComponent(target_lang)}`;
 
@@ -67,18 +77,39 @@ export async function translateAPI(text: string, target_lang: string): Promise<s
 
 }
 
+// +=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+//
+//  Runs through every single dialogue in the msg category and sees if splitting    //
+//  and reassembling returns the exact same result                                  //
+//  Input: The folder of map files and the list of MAP000 files                     //
+//  Output: false if a string doesn't match, true if they all pass                  //
+// +=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+//
 export function IsDisassembleValid(mapfile_folder : string, matching_files : string[], languages : string[]): boolean {
     let scraped_data = ScrapeAll(mapfile_folder, matching_files);
     let output_JSON = TranslateAll(languages, scraped_data);
     let mismatchCount = 0;
+    let nonmatchingStrings = [];
 
     for (let key in output_JSON.msg) {
         const returnedArray = splitDialogue(String(output_JSON.msg[key]));
         const finalString = returnedArray.join("");
         if (String(output_JSON.msg[key]) !== finalString) {
+            nonmatchingStrings.push([String(output_JSON.msg[key]), finalString]);
             mismatchCount++;
         }
     }
 
-    return mismatchCount > 0 ? false : true;
+    if (mismatchCount > 0) {
+        console.log(`Disassembly/Reassembly failed. ${mismatchCount} strings did not match.`);
+        nonmatchingStrings.forEach((item) => {
+            console.log(`Original: ${item[0]}`);
+            console.log(`Original Length: ${item[0].length}`);
+            console.log(`Reassembled: ${item[1]}`);
+            console.log(`Reassembled Length: ${item[1].length}\n`);
+        });
+        return false;
+    } 
+    else {
+        console.log("All string successfully passed.");
+        return true;
+    }
 }
