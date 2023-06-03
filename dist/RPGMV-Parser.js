2b6857a4da7331d77f0b0d476091e778460d3857
LilacVenus <66577598+lilacvenus@users.noreply.github.com>
2023-06-03 17:34:51 -0300

    Welcome in Electron

--- dist/RPGMV-Parser.js
+++ dist/RPGMV-Parser.js
@@ -1,9 +1,13 @@
-import { IsDisassembleValid } from './UserFunctions.js';
-import { readdirSync } from 'fs';
-const languages = ["Français"];
-const mapfile_folder = "./MapFiles/";
-const matching_files = readdirSync(mapfile_folder).filter(file => file.match(/^Map\d{3}\.json$/)); // Regex to match MAP000.json files
-IsDisassembleValid(mapfile_folder, matching_files, languages);
+"use strict";
+// import { OutputTranslations, OutputNotTranslated, OutputOverTranslated, OutputUnderTranslated } from './OutputFunctions';
+// import { splitDialogue, translateAPI, IsDisassembleValid } from './UserFunctions.js';
+// import { ScrapeMessages } from './ScrapeFunctions.js';
+// import { Translate } from './TranslateFunctions.js';
+// import { readdirSync } from 'fs';
+// const languages: string[] = ["Français"];
+// const mapfile_folder: string = "./MapFiles/";
+// const matching_files = readdirSync(mapfile_folder).filter(file => file.match(/^Map\d{3}\.json$/)); // Regex to match MAP000.json files
+// IsDisassembleValid(mapfile_folder, matching_files, languages);
 // let scraped_data = ScrapeMessages(mapfile_folder, matching_files);
 // let output_JSON = Translate(languages, scraped_data, "msg");
 // let array = splitDialogue(key);
@@ -15,4 +19,30 @@ IsDisassembleValid(mapfile_folder, matching_files, languages);
 //                 }
 //         }
 // });
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        icon: 'media/egg.ico',
+        frame: false,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
+    });
+    win.loadFile('index.html');
+};
+app.whenReady().then(() => {
+    createWindow();
+    console.log(__dirname);
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0)
+            createWindow();
+    });
+});
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin')
+        app.quit();
+});
 //# sourceMappingURL=RPGMV-Parser.js.map
