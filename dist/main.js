a3c100ed22af9a3145cdf1291d8ff64cc973d8e9
LilacVenus <66577598+lilacvenus@users.noreply.github.com>
2023-06-07 13:30:58 -0300

    Cleaning up ignores

--- dist/main.js
+++
@@ -1,7 +0,0 @@
-import { OutputNotTranslated } from './OutputFunctions.js';
-const languages = ["Français"];
-// OutputTranslations(languages);
-// OutputUnderTranslated(languages);
-// OutputOverTranslated(languages);
-OutputNotTranslated();
-//# sourceMappingURL=main.js.map
